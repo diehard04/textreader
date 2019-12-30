5feb5695bea699e2a9ab8a2ce754750e75d17211
Dipak Kumar Mehta <dipakm@invendis.com>
2019-12-30 12:21:51 +0530

    firsr commit

---
+++ app/src/main/java/com/example/textreader/ui/MainFragment.java
@@ -0,0 +1,216 @@
+package com.example.textreader.ui;
+
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.textreader.MainActivity;
+import com.example.textreader.R;
+import com.example.textreader.utils.CommonUtils;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
+
+public class MainFragment extends Fragment implements View.OnClickListener{
+
+    private MainViewModel mViewModel;
+    private Bitmap myBitmap;
+    private ImageView myImageView;
+    private TextView myTextView;
+    public static final int WRITE_STORAGE = 100;
+    public static final int SELECT_PHOTO = 102;
+    public File photo;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+        myTextView = view.findViewById(R.id.textView);
+        myImageView = view.findViewById(R.id.imageView);
+        view.findViewById(R.id.checkText).setOnClickListener(this);
+        view.findViewById(R.id.select_image).setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case WRITE_STORAGE:
+                    checkPermission(requestCode);
+                    break;
+                case SELECT_PHOTO:
+                    Uri dataUri = data.getData();
+                    String path = CommonUtils.getPath(getContext(), dataUri);
+                    if (path == null) {
+                        myBitmap = CommonUtils.resizePhoto(photo, getContext(), dataUri, myImageView);
+                    } else {
+                        myBitmap = CommonUtils.resizePhoto(photo, path, myImageView);
+                    }
+                    if (myBitmap != null) {
+                        myTextView.setText(null);
+                        myImageView.setImageBitmap(myBitmap);
+                    }
+                    break;
+
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.checkText:
+                if (myBitmap != null) {
+                    runTextRecognition();
+                }
+                break;
+            case R.id.select_image:
+                checkPermission(WRITE_STORAGE);
+                break;
+        }
+    }
+
+    private void runTextRecognition() {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(myBitmap);
+        FirebaseVisionTextDetector detector = FirebaseVision.getInstance().getVisionTextDetector();
+        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText texts) {
+                processExtractedText(texts);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure
+                    (@NonNull Exception exception) {
+                System.out.println(exception);
+                Toast.makeText(getContext(),
+                        "Exception", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void processExtractedText(FirebaseVisionText firebaseVisionText) {
+        myTextView.setText(null);
+        if (firebaseVisionText.getBlocks().size() == 0) {
+            myTextView.setText("no text");
+            return;
+        }
+        for (FirebaseVisionText.Block block : firebaseVisionText.getBlocks()) {
+            myTextView.append(block.getText());
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case WRITE_STORAGE:
+
+                //If the permission request is granted, then...//
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //...call selectPicture//
+                    selectPicture();
+                    //If the permission request is denied, then...//
+                } else {
+                    //...display the “permission_request” string//
+                    requestPermission(getActivity(), requestCode, "plese give access to your galary");
+                }
+                break;
+
+        }
+    }
+
+    //Display the permission request dialog//
+    public static void requestPermission(final Activity activity, final int requestCode, String msg) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(activity);
+        alert.setMessage(msg);
+        alert.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                Intent permissonIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                permissonIntent.setData(Uri.parse("package:" + activity.getPackageName()));
+                activity.startActivityForResult(permissonIntent, requestCode);
+            }
+        });
+        alert.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        alert.setCancelable(false);
+        alert.show();
+    }
+
+    //Check whether the user has granted the WRITE_STORAGE permission//
+    private void checkPermission(int requestCode) {
+        switch (requestCode) {
+            case WRITE_STORAGE:
+                int hasWriteExternalStoragePermission = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                //If we have access to external storage...//
+                if (hasWriteExternalStoragePermission == PackageManager.PERMISSION_GRANTED) {
+                    //...call selectPicture, which launches an Activity where the user can select an image//
+                    selectPicture();
+                    //If permission hasn’t been granted, then...//
+                } else {
+                    //...request the permission//
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, requestCode);
+                }
+                break;
+        }
+    }
+
+
+    private void selectPicture() {
+        photo = CommonUtils.createTempFile(photo);
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        //Start an Activity where the user can choose an image//
+        startActivityForResult(intent, SELECT_PHOTO);
+    }
+}
---
+++ app/src/main/java/com/example/textreader/ui/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.textreader.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
